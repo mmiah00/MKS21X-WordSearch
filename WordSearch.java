e05d0b7cdd4fe7f79bcec25f0f12b456e4e2ad12
mmiah00 <mmiah00@stuty.edu>
2018-11-09 10:07:52 -0500

    wrote addWord

--- WordSearch.java
+++ WordSearch.java
@@ -121,4 +121,45 @@ public class WordSearch{
      }
      return true;
    }
+
+   /**Attempts to add a given word to the specified position of the WordGrid.
+  *The word is added in the direction rowIncrement,colIncrement
+
+  *Words must have a corresponding letter to match any letters that it overlaps.
+
+  *
+  *@param word is any text to be added to the word grid.
+  *@param row is the vertical locaiton of where you want the word to start.
+  *@param col is the horizontal location of where you want the word to start.
+
+  *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+  *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+
+  *@return true when: the word is added successfully.
+  *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+  *        OR there are overlapping letters that do not match
+  */
+ public boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+   int x = col;
+   int y = row;
+   if (x + word.length () - 1 > data[row].length || y + word.length () - 1 > data.length) {
+     return false;
+   }
+   int index = 0;
+   while (index < word.length ()) {
+     data[y][x] = word.charAt (index);
+     x += colIncrement;
+     y += rowIncrement;
+     index += 1;
+   }
+   return true;
+   }
+ }
+
+ /*[rowIncrement,colIncrement] examples:
+
+  *[-1,1] would add up to the right because (row -1 each time, col + 1 each time)
+
+  *[1,0] would add to the right because (row+1), with no col change
+  */
 }
