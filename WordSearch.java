3fc87fc641f08fb1ec41c5bb709c3e0f4e3a08b7
mmiah <mmiah00@stuy.edu>
2018-11-11 21:59:21 -0500

    wrote addWord

--- WordSearch.java
+++ WordSearch.java
@@ -81,6 +81,49 @@ public class WordSearch{
       return ans;
     }
 
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added in the direction rowIncrement,colIncrement
+     *Words must have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+     *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+     *@return true when: the word is added successfully.
+     *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+     *        OR there are overlapping letters that do not match
+     */
+    private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+      if (rowIncrement == 0 && colIncrement == 0) { return false;}
+      if (row + rowIncrement * word.length () > data.length - 1) { return false;}
+      if (col + colIncrement * word.length () > data[row].length - 1) { return false;}
+      int index = 0;
+      int x = col;
+      int y = row;
+      while (index < word.length ()) {
+        data[y][x] = word.charAt (index);
+        x += colIncrement;
+        y += rowIncrement;
+        index += 1;
+      }
+      return true;
+    }
+
+    /*[rowIncrement,colIncrement] examples:
+     *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+     *[ 1,0] would add downwards because (row+1), with no col change
+     *[ 0,-1] would add towards the left because (col - 1), with no row change
+     */
+
+
+
+
+
+
+
+
+    //***********************************************************************************************************\\`
 
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid, and must
@@ -93,6 +136,8 @@ public class WordSearch{
      * or there are overlapping letters that do not match, then false is returned
      * and the board is NOT modified.
      */
+
+    /*
     public boolean addWordHorizontal(String word,int row, int col){
       if (col + word.length () - 1> data[row].length - 1) {
         return false;
@@ -109,6 +154,7 @@ public class WordSearch{
       }
       return true;
     }
+    */
 
    /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from top to bottom, must fit on the WordGrid, and must
@@ -121,6 +167,8 @@ public class WordSearch{
      *or there are overlapping letters that do not match, then false is returned.
      *and the board is NOT modified.
      */
+
+    /*
     public boolean addWordVertical(String word,int row, int col){
       if (row + word.length () > data.length - 1) {
         return false;
@@ -137,6 +185,7 @@ public class WordSearch{
       }
       return true;
     }
+    */
 
     /**Attempts to add a given word to the specified position of the WordGrid.
       *The word is added from top left to bottom right, must fit on the WordGrid,
@@ -148,6 +197,8 @@ public class WordSearch{
       *@return true when the word is added successfully. When the word doesn't fit,
       *or there are overlapping letters that do not match, then false is returned.
       */
+
+  /*
    public boolean addWordDiagonal(String word,int row, int col){
      int x = col;
      int y = row;
@@ -163,6 +214,9 @@ public class WordSearch{
      }
      return true;
    }
+   */
+
+   //**********************************************************************************************************************\\
 
    /**Attempts to add a given word to the specified position of the WordGrid.
   *The word is added in the direction rowIncrement,colIncrement
@@ -181,6 +235,13 @@ public class WordSearch{
   *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
   *        OR there are overlapping letters that do not match
   */
+  /*[rowIncrement,colIncrement] examples:
+
+   *[-1,1] would add up to the right because (row -1 each time, col + 1 each time)
+
+   *[1,0] would add to the right because (row+1), with no col change
+   */
+/*
  public boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
    int x = col;
    int y = row;
@@ -199,11 +260,6 @@ public class WordSearch{
    }
    return true;
   }
-}
-
- /*[rowIncrement,colIncrement] examples:
-
-  *[-1,1] would add up to the right because (row -1 each time, col + 1 each time)
-
-  *[1,0] would add to the right because (row+1), with no col change
   */
+  //**********************************************************************************************************************\\
+}
